144c0eb5e54f22d24655dbdd4a764c783186ba77
sy <7525117+huolingmengling@user.noreply.gitee.com>
2021-07-27 17:44:41 +0800

    添加config.json

--- vue.config.js
+++ vue.config.js
@@ -1,17 +1,26 @@
 //vue.config.js
-const TransformPages = require('uni-read-pages')
-const { webpack } = new TransformPages()
-module.exports = {
-  configureWebpack: {
-    plugins: [
-      new webpack.DefinePlugin({
-        ROUTES: webpack.DefinePlugin.runtimeValue(() => {
-          const tfPages = new TransformPages({
-            includes: ['path', 'aliasPath', 'name', 'meta', 'title'] //需要获取包涵的字段
-          });
-          return JSON.stringify(tfPages.routes)
-        }, true)
-      })
-    ]
-  }
+const TransformPages = require('uni-read-pages');
+const { webpack } = new TransformPages();
+const path = require('path');
+function resolve(dir) {
+	return path.join(__dirname, dir);
 }
+module.exports = {
+	configureWebpack: {
+		plugins: [
+			new webpack.DefinePlugin({
+				ROUTES: webpack.DefinePlugin.runtimeValue(() => {
+					const tfPages = new TransformPages({
+						includes: ['path', 'aliasPath', 'name', 'meta', 'title'], //需要获取包涵的字段
+					});
+					return JSON.stringify(tfPages.routes);
+				}, true),
+			}),
+		],
+		resolve: {
+			alias: {
+				'@': resolve(''),
+			},
+		},
+	},
+};
